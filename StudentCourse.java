417421f9f7f1e4451221c25299fde8b4eadce2e8
Chaya <gangalchaya@gmail.com>
2024-01-29 18:54:35 +0530

    Code to print student details and course details

--- StudentCourse.java
+++ StudentCourse.java
@@ -1,5 +1,5 @@
 class StudentCourse {
-    private String[] courses = { "C#", "Software Testing ", "Machine Learning", "DEVOPS", "Minor Project" };
+    private String[] courses = { "C#", "Software Testing ", "Machine  Learning", "DEVOPS", "Minor Project" };
     private int[] marks = { 88, 92, 95, 98, 91 };
 
     public void displayStudentcourses() {
